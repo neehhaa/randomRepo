4823207d7c4efc48bde3a57928e4997a1a569dc4
neehhaa <nehawasnik95@outlook.com>
2022-06-30 00:15:24 +0530

    Greatest number of two

---
+++ Random/Greater.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Greater {
+
+    public int greaterNum(int num1, int num2){
+        int great = 0;
+        if(num1>num2){
+            great = num1;
+        }
+        else{
+            great = num2;
+        }
+        return great;
+    }
+    public static void main(String[] args){
+
+        System.out.println("Enter two numbers : ");
+        Greater g = new Greater();
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("Greatest number is : "+g.greaterNum(num1,num2));
+
+    }
+}
